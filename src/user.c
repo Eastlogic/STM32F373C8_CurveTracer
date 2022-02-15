762a7c5b363e5477b6114a1b80cf716bac1da506
Eastlogic <eastlogic@yandex.ru>
2022-02-15 20:56:07 +0200

    Added some code to shift reg function

--- src/user.c
+++ src/user.c
@@ -21,18 +21,20 @@ GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_OUT;
 GPIO_InitStruct.GPIO_Speed = GPIO_Speed_Level_1;
 GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-
 GPIO_Init(SI_GPIO, &GPIO_InitStruct);
 
 // Serial clock for 74HC595 shift register
 GPIO_InitStruct.GPIO_Pin   = SCLK_GPIO_Pin;
-
 GPIO_Init(SCLK_GPIO, &GPIO_InitStruct);
 
 // Latch clock for 74HC595 shift register
 GPIO_InitStruct.GPIO_Pin   = LCLK_GPIO_Pin;
-
 GPIO_Init(LCLK_GPIO, &GPIO_InitStruct);
+
+// Reset control lines to "low" on init phase
+GPIO_ResetBits(SI_GPIO, SI_GPIO_Pin);
+GPIO_ResetBits(SCLK_GPIO, SCLK_GPIO_Pin);
+GPIO_ResetBits(LCLK_GPIO, LCLK_GPIO_Pin);
 
 // Set all shift reg. outputs to "low" on init phase
 SetShiftRegOut(K_NULL);
@@ -55,20 +57,21 @@ uint8_t j;
 	{
 		if ( (val<<j) & 0x8000)	//Set "high" on data line
 		{
-		
+		GPIO_SetBits(SI_GPIO, SI_GPIO_Pin);
 		}
 		else										//Set "low" on data line
 		{
-		
+		GPIO_ResetBits(SI_GPIO, SI_GPIO_Pin);
 		}
+	
 	// Form rising edge on shift clock (SCLK) line -> set "high", then "low"
-	
-	
+	GPIO_SetBits(SCLK_GPIO, SCLK_GPIO_Pin);
+	GPIO_ResetBits(SCLK_GPIO, SCLK_GPIO_Pin);
 	}
 	
 	// Form rising edge on latch clock (LCLK) line -> set "high", then "low"
-
-
+	GPIO_SetBits(LCLK_GPIO, LCLK_GPIO_Pin);
+	GPIO_ResetBits(LCLK_GPIO, LCLK_GPIO_Pin);
 }
 // *************************************************************
 
