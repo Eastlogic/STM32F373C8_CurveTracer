c5ef4817ca0acf29edbcdf8f8788fc6505302bd2
Eastlogic <eastlogic@yandex.ru>
2022-02-15 13:33:38 +0200

    New project files added

---
+++ src/user.h
@@ -0,0 +1,13 @@
+#ifndef USER_H_
+#define USER_H_
+
+
+// -- System clocks init -------------------------------
+void RCC_init(void);
+
+// -- GPIOs init ---------------------------------------
+void GPIO_init(void);
+
+
+
+#endif
