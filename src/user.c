a67700ee8439daa4c1fab6658dc8865f4bb6fb3b
Eastlogic <eastlogic@yandex.ru>
2022-02-15 16:08:23 +0200

    Added some new code inside

--- src/user.c
+++ src/user.c
@@ -1,17 +1,44 @@
 #include "user.h"
 #include "stm32f37x.h"
 
-// -- System clocks init -------------------------------------------------------
+// -- System clocks init --------------------------------------
 void RCC_init(void)
 {
 
 }
 
 
-// -- GPIOs init ---------------------------------------------------------------
+// -- GPIOs init ----------------------------------------------
 void GPIO_init(void)
 {
 
 }
-// *****************************************************************************
+
+
+// -- 74HC595 Port Extender Set Out Value ---------------------
+void SetShiftRegOut (uint16_t val)
+{
+uint8_t j;
+
+	// Shift out data to register
+	for (j=0; j++; j<16)
+	{
+		if ( (val<<j) & 0x8000))	//Set "high" on data line
+		{
+		
+		}
+		else						//Set "low" on data line
+		{
+		
+		}
+	// Form rising edge on shift clock (SCLK) line -> set "high", then "low"
+	
+	
+	}
+	
+	// Form rising edge on latch clock (LCLK) line -> set "high", then "low"
+
+
+}
+// *************************************************************
 
