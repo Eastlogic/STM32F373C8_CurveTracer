be06c5c22080764a8cb8aff70ab01063be3c4a39
Eastlogic <eastlogic@yandex.ru>
2022-02-17 19:00:36 +0200

    Added macro defs for StepAmp state control

--- src/user.h
+++ src/user.h
@@ -44,6 +44,7 @@ void GPIO_init(void);
 #define K_NULL	(uint16_t) (0x0000)
 
 
+
 // -- 74HC595 Port Extender Set Out Value --------------------------
 // Function usage like this:		ShiftRegSetOut(K1 | K3 | K9);
 // Will set K... and reset other bits to "low"
@@ -55,5 +56,62 @@ void ShiftRegSetBits(uint16_t val);
 
 // -- 74HC595 Reset Out Bits (without changing others) -------------
 void ShiftRegResetBits(uint16_t val);
+
+
+// -- Definitions for Step Amplifier state vontrol -----------------
+
+#define ST_AMP_POSITIVE				ShiftRegResetBits(K1);
+#define ST_AMP_NEGATIVE				ShiftRegSetBits(K1);
+
+#define ST_AMP_I_RANGE1				ShiftRegResetBits(K3|K4|K5|K6);\
+															ShiftRegSetBits(K2);
+
+#define ST_AMP_I_RANGE2				ShiftRegResetBits(K2|K3|K4|K5|K6);
+
+#define ST_AMP_I_RANGE3				ShiftRegResetBits(K3|K4|K6);\
+															ShiftRegSetBits(K2|K5);
+
+#define ST_AMP_I_RANGE4				ShiftRegResetBits(K2|K3|K4|K6);\
+															ShiftRegSetBits(K5);
+
+#define ST_AMP_I_RANGE5				ShiftRegResetBits(K3|K5|K6);\
+															ShiftRegSetBits(K2|K4);
+
+#define ST_AMP_I_RANGE6				ShiftRegResetBits(K2|K3|K5|K6);\
+															ShiftRegSetBits(K4);
+
+#define ST_AMP_I_RANGE7				ShiftRegResetBits(K3|K5);\
+															ShiftRegSetBits(K2|K4|K6);
+
+#define ST_AMP_I_RANGE8				ShiftRegResetBits(K2|K3|K5);\
+															ShiftRegSetBits(K4|K6);
+
+#define ST_AMP_V_RANGE1				ShiftRegResetBits(K4|K5|K6);\
+															ShiftRegSetBits(K2|K3);
+
+#define ST_AMP_V_RANGE2				ShiftRegResetBits(K2|K4|K5|K6);\
+															ShiftRegSetBits(K3);
+
+#define ST_AMP_V_RANGE3				ShiftRegResetBits(K4|K6);\
+															ShiftRegSetBits(K2|K3|K5);
+
+#define ST_AMP_V_RANGE4				ShiftRegResetBits(K2|K4|K6);\
+															ShiftRegSetBits(K3|K5);
+
+#define ST_AMP_V_RANGE5				ShiftRegResetBits(K5|K6);\
+															ShiftRegSetBits(K2|K3|K4);
+
+#define ST_AMP_V_RANGE6				ShiftRegResetBits(K2|K5|K6);\
+															ShiftRegSetBits(K3|K4);
+
+#define ST_AMP_V_RANGE7				ShiftRegResetBits(K5);\
+															ShiftRegSetBits(K2|K3|K4|K6);
+
+#define ST_AMP_V_RANGE8				ShiftRegResetBits(K2|K5);\
+															ShiftRegSetBits(K3|K4|K6);
+
+
+
+
 
 #endif
